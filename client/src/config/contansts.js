b3fa370e60ed5ae892426baafc32663588f9ac65
kjh <i1004902@naver.com>
2024-02-01 14:40:07 +0900

    로벨4

--- client/src/config/contansts.js
+++ client/src/config/contansts.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://www.i1004902.com/app1";
+export const API_URL = "https://www.i1004902.com/app1";
 // export const API_URL = "http://3.37.182.254/app1";
 // export const API_URL = "http://localhost:8000"; 
